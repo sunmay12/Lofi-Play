7e9298674789752af0611491a5d5489ac42c98a9
Minseo <kimalstj2739@naver.com>
2025-06-18 19:22:40 +0900

    Initial Commit

---
+++ Lofi-Play/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lofi-Play
+//
+//  Created by 김민서 on 6/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
